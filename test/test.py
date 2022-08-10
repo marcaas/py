449bf15da004604db891a08f133eebb759288373
marcaas <t13274515361@gmail.com>
2022-08-10 15:53:22 +0800

    modified:   test/test.py

--- test/test.py
+++ test/test.py
@@ -1,20 +1,20 @@
-# from unittest import result
+from unittest import result
 import array
 
-# result = 0
-# for i in range(100):
-#     result += i
-# x = 4
-# x = "four"
-# print(x)
-# L = list(range(10))
-# for i in L:
-#     print(L[i])
-# L2 = [str(c) for c in L]
-# for i in range(10):
-#     print(L2[i])
-# L3 = [True,"2",3.0,4]
-# print([type(item)for item in L3])
-# L = list(range(10))
-# A = array.array('i',L)
-# print(A)
+result = 0
+for i in range(100):
+    result += i
+x = 4
+x = "four"
+print(x)
+L = list(range(10))
+for i in L:
+    print(L[i])
+L2 = [str(c) for c in L]
+for i in range(10):
+    print(L2[i])
+L3 = [True,"2",3.0,4]
+print([type(item)for item in L3])
+L = list(range(10))
+A = array.array('i',L)
+print(A)
